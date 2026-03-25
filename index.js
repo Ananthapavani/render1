b5ede2054c5b5acf6046069cc1001de30f9115df
Ananthapavani <musiniananthapavani@gmail.com>
2026-03-25 20:31:07 +0530

    updated text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Dinga! Your app is live 🎉");
+  res.send("Welcome to Devops.");
 });
 
 const PORT = process.env.PORT || 3000;
